0ada2df1b10fc4998a5a7423b395daf3c01e037b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-05-24 15:16:48 +0200

    sm8250-common: Switch to Xiaomi SPI IR HIDL

--- kona.mk
+++ kona.mk
@@ -148,8 +148,7 @@ PRODUCT_PACKAGES += \
 
 # Consumer IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir@1.0-service.xiaomi-spi
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
